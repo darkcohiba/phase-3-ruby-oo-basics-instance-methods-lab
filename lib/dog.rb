8bfba7520fba3f844e8e25bceb7b4a250cc07fd5
Sam <samuel.waters@gmail.com>
2022-04-05 10:22:02 -0600

    easy

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+    # Class body
+    # Instance Method Definition
+    def bark
+        puts "Woof!"
+    end
+    def sit
+        puts "the dog sat!"
+    end
+end
+
+fido = Dog.new
+fido.bark
+fido.sit
